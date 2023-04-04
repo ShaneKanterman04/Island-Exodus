43b2086fe5469cf506d7185d0e23248ce2cf1e0c
ShaneKanterman04 <122749779+ShaneKanterman04@users.noreply.github.com>
2023-04-04 19:55:37 -0400

    Map/Sprinting
    created a basic test map and sprinting

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 100
+@export var sprint_mult : float = 2
 
 func _physics_process(delta):
 	# get input direction
@@ -8,9 +9,14 @@ func _physics_process(delta):
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
+	var sprint = Input.get_action_strength("sprint")
 	
 	#update vel
-	velocity = input_direction * move_speed
+	if sprint == 1: #sprinting
+		velocity = input_direction * move_speed * sprint_mult
+	else:  #not sprinting
+		velocity = input_direction * move_speed
+	
 	
 	#move
 	move_and_slide()
